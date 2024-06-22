b7a8fb734426c50cc9a28e35e52141fc03911981
AJAY KUMAR SARASWAT <ajaykumarsaraswat9045@gmail.com>
2024-06-22 17:01:17 +0530

    complete

--- YOUTUBE-BLOG/app.js
+++ YOUTUBE-BLOG/app.js
@@ -1,15 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
-const userRoute  = require('./routes/user') ;
+const userRoute  = require('./routes/user.js') ;
+const blogRoute = require('./routes/blog.js')
 const { connectTOMongoDB } = require("./connection.js");
 const cookieParser = require("cookie-parser");
-const { checkForAuthenticationCookie } = require("./middlewares/authentication.js")
+const { checkForAuthenticationCookie } = require("./middlewares/authentication.js");
+const Blog =  require('./models/blog.js');
+
 
 
 const app = express();
-const PORT = 8000;
+const PORT =process.env.PORT || 8000;
 
-connectTOMongoDB("mongodb://127.0.0.1:27017/blogify")
+connectTOMongoDB(process.env.MONGO_URL)
 .then(()=>{
 console.log("mogoDb connected");
 });
@@ -20,16 +25,20 @@ app.set("views",path.resolve("./views"));
 app.use(express.urlencoded({extended : false}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public"))); // to load image on cards we make upload as static
 
+app.get('/',async (req,res)=>{
+    const allBlogs = await Blog.find({});
 
-app.get('/',(req,res)=>{
     
      res.render("home",{
         user : req.user,
+        blogs : allBlogs,
     });
 
 });
 app.use("/user",userRoute);
+app.use('/blog',blogRoute);
 
 
 app.listen(PORT,(req,res)=>{
